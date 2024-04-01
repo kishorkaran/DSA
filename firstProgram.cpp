afc13a1b627a0181800e5fa60f4144e3eabec09c
Karan Kishor <89378732+karanthek2002@users.noreply.github.com>
2024-04-01 23:04:34 +0530

    first program

---
+++ firstProgram.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // headerfiles which contains definitions of predefined functions
+
+using namespace std;
+// namespace is similar to section, in different namespaces the implementions can differ ex - cout
+
+int main()
+{
+    // Execution of a program is started from main() function in C++
+    // std::cout<<"This is my First C++ program";
+
+    cout << "This is my First C++ program";
+    return 0; 
+}
